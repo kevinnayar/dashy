cfcbed5636409d67de6f09852032eef93d673d34
Kevin Nayar <kevin.nayar@gmail.com>
2023-01-28 14:22:47 -0600

    fix file paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@tremor/react/dist/esm/tremor.css';
-import '@/styles/globals.css';
+import '@dashy/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
